07ad87de98ee2efcd028747c51814b07f78e748f
utkuzun <utkuzun50@gmail.com>
2022-08-02 15:43:28 +0300

    procfile added

--- index.js
+++ index.js
@@ -99,6 +99,6 @@ app.delete("/api/persons/:id",(req, res)=> {
   res.end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
